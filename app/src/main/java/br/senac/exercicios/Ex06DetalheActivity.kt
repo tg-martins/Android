db6fac921886e0abea44e08c471d109b9ee47646
Thiago Martins <contato.thiagocesar@icloud.com>
2021-04-11 20:59:13 -0300

    exercise 06

---
+++ app/src/main/java/br/senac/exercicios/Ex06DetalheActivity.kt
@@ -0,0 +1,21 @@
+package br.senac.exercicios
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senac.exercicios.databinding.ActivityEx06DetalheBinding
+
+class Ex06DetalheActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityEx06DetalheBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEx06DetalheBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val img = intent.getIntExtra("img",0)
+        binding.imagem.setImageResource(img)
+        binding.imagem.tag = img
+    }
+}
